fb9f471193cf567c0634d160f6bf8b1884ba934e
wenziwen <wenzw@zdnst.com>
2017-06-22 10:31:00 +0800

    完成基本功能

---
+++ app/src/main/java/com/ziwenwen/onekeychat/MainActivity.java
@@ -0,0 +1,190 @@
+package com.ziwenwen.onekeychat;
+
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Parcelable;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etName;
+    CheckBox cbVideoChat;
+
+    String name;
+    boolean isVideoChat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 有名字时表示桌面快捷方式进来的, 直接打开微信聊天
+        if (getIntent().hasExtra("name")) {
+            name = getIntent().getStringExtra("name");
+            isVideoChat = getIntent().getBooleanExtra("isVideoChat", false);
+            oneKeyChat(name, isVideoChat);
+            finish();
+            return;
+        }
+        // 保存数据后下一次直接显示拨打按钮
+        SharedPreferences preferences = openSharedPreferences();
+        name = preferences.getString("name", null);
+        isVideoChat = preferences.getBoolean("isVideoChat", false);
+        if (!TextUtils.isEmpty(name)) {
+            showOneKeyBtn();
+        }
+
+        etName = (EditText) findViewById(R.id.et_name);
+        cbVideoChat = (CheckBox) findViewById(R.id.cb_video_chat);
+        cbVideoChat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isVideoChat = isChecked;
+            }
+        });
+
+        findViewById(R.id.btn_open_accessibility)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                        startActivity(intent);
+                    }
+                });
+        findViewById(R.id.btn_test)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        oneKeyChat(etName.getText().toString(), isVideoChat);
+                    }
+                });
+
+        findViewById(R.id.btn_create_shortcut)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        createShotCut(MainActivity.this, MainActivity.class, etName.getText().toString(), isVideoChat);
+                    }
+                });
+        findViewById(R.id.btn_one_key_chat)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        oneKeyChat(name, isVideoChat);
+                    }
+                });
+        findViewById(R.id.btn_complete)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        name = etName.getText().toString();
+                        if (TextUtils.isEmpty(name)) {
+                            Toast.makeText(MainActivity.this, "请输入微信昵称", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        isVideoChat = cbVideoChat.isChecked();
+                        SharedPreferences preferences = openSharedPreferences();
+                        preferences.edit()
+                                .putString("name", name)
+                                .putBoolean("isVideoChat", isVideoChat)
+                                .apply();
+                        showOneKeyBtn();
+                    }
+                });
+    }
+
+    private SharedPreferences openSharedPreferences() {
+        return getSharedPreferences("cache", MODE_PRIVATE);
+    }
+
+    private void showOneKeyBtn() {
+        findViewById(R.id.btn_one_key_chat)
+                .setVisibility(View.VISIBLE);
+        findViewById(R.id.ll_setting)
+                .setVisibility(View.GONE);
+    }
+
+    private void showSettingBtn() {
+        findViewById(R.id.btn_one_key_chat)
+                .setVisibility(View.GONE);
+        findViewById(R.id.ll_setting)
+                .setVisibility(View.VISIBLE);
+    }
+
+    private void oneKeyChat(String name, boolean isVideoChat) {
+        try {
+            // 启动监听
+            Intent intent = new Intent(MainActivity.this, MyAccessibility.class);
+            intent.putExtra("name", name);
+            intent.putExtra("isVideoChat", isVideoChat);
+            startService(intent);
+            // 打开微信首页
+            intent = new Intent();
+            ComponentName cmp = new ComponentName("com.tencent.mm", "com.tencent.mm.ui.LauncherUI");
+            intent.setAction(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setComponent(cmp);
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            // 确保没有微信时不会崩溃
+            Toast.makeText(this, "请安装好微信", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void createShotCut(Context context, Class<?> clazz, String name, boolean isVideoChat) {
+
+        Intent shortcutIntent = new Intent(Intent.ACTION_MAIN);
+
+        shortcutIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        shortcutIntent.setClass(context, clazz);
+        /**
+         * 创建一个Bundle对象让其保存将要传递的值
+         */
+        Bundle bundle = new Bundle();
+        bundle.putString("name", name);
+        bundle.putBoolean("isVideoChat", isVideoChat);
+        shortcutIntent.putExtras(bundle);
+        /**
+         * 设置这条属性，可以使点击快捷方式后关闭其他的任务栈的其他activity，然后创建指定的acticity
+         */
+        shortcutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Intent shortcut = new Intent(Intent.ACTION_CREATE_SHORTCUT);
+        shortcut.putExtra("duplicate", false);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+
+        Parcelable icon = Intent.ShortcutIconResource.fromContext(
+                getApplicationContext(), R.mipmap.ic_launcher);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+        shortcut.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        context.sendBroadcast(shortcut);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("打开设置");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        showSettingBtn();
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/ziwenwen/onekeychat/MyAccessibility.java
@@ -0,0 +1,210 @@
+package com.ziwenwen.onekeychat;
+
+import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+public class MyAccessibility extends AccessibilityService {
+
+    private static final String TAG = "MyAccessibility";
+    private String name;
+    private boolean isVideoChat;
+    int currentStep =  -1;
+
+    @Override
+    public void onInterrupt() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null) {
+            currentStep = 0;
+            name = intent.getStringExtra("name");
+            isVideoChat = intent.getBooleanExtra("isVideoChat", false);
+            Log.d(TAG, "onStartCommand:" + name);
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        // 确定需要时才执行
+        if (currentStep > 3 || currentStep < 0) {
+            return;
+        }
+        Log.d(TAG, "onAccessibilityEvent: " + event.toString());
+        String className = event.getClassName().toString();
+        Log.d(TAG, "onAccessibilityEvent class: " + className);
+        boolean disable = TextUtils.isEmpty(name);
+        if (disable) {
+            return;
+        }
+        int eventType = event.getEventType();
+        switch (eventType) {
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+//                if (className.equals("com.tencent.mm.ui.LauncherUI")) { // 进入首页, 点击对应的用户. 因为列表刷新有延迟, 需要sleep一下
+//                    try {
+//                        Thread.sleep(2000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                    findViewByNameAndClick(name);//state change
+//                } else
+//                if (className.equals("com.tencent.mm.ui.base.k")) {// 视频语音聊天选择
+//                    sleep(1000);
+//                    findViewByNameAndClick("语音聊天");
+//                    name = null; // 只点击一次
+//                }
+//                break;
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                if (0 == currentStep) {
+                    AccessibilityNodeInfo node = findNodeByName(getRootInActiveWindow(), name);
+                    if (node != null) {
+                        Log.d(TAG, "step 0 finish");
+                        clickNodeAndParent(node);
+                        currentStep++;
+                    }
+                } else if (1 == currentStep) {
+                    // 找到更多按钮
+                    AccessibilityNodeInfo node = findNodeByDescription(getRootInActiveWindow(), "更多功能按钮，");
+                    if (node != null) {
+                        Log.d(TAG, "step 1 finish");
+                        clickNodeAndParent(node);
+                        currentStep++;
+                    }
+                } else if (2 == currentStep) {
+                    // 找到视频聊天
+                    AccessibilityNodeInfo node = findNodeByName(getRootInActiveWindow(), "视频聊天");
+                    if (node != null) {
+                        Log.d(TAG, "step 2 finish");
+                        clickNodeAndParent(node);
+                        currentStep++;
+                    }
+                } else if (3 == currentStep) {
+                    // 找到视频聊天
+                    AccessibilityNodeInfo node;
+                    if (isVideoChat) {
+                        node = findNodeByName(getRootInActiveWindow(), "视频聊天");
+                    } else {
+                        node = findNodeByName(getRootInActiveWindow(), "语音聊天");
+                    }
+                    if (node != null) {
+                        Log.d(TAG, "step 3 finish");
+                        clickNodeAndParent(node);
+                        currentStep++;
+                        name = null;
+                    }
+                }
+                break;
+        }
+    }
+
+    /**
+     * 模拟点击
+     */
+    private boolean findViewByNameAndClick(String name) {
+        Log.d(TAG, "findViewByNameAndClick: " + name);
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        AccessibilityNodeInfo node = findNodeByName(rootNode, name);
+
+        if (node == null) {
+            Log.d(TAG, "findViewByNameAndClick fail: " + name);
+            return false;
+        } else {
+            clickNodeAndParent(node);
+            return true;
+        }
+    }
+
+    /**
+     * 找到对应的控件并且点击
+     *
+     * @param name
+     */
+    private void findViewByDescriptionAndClick(String name) {
+        Log.d(TAG, "findViewByDescriptionAndClick: " + name);
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        AccessibilityNodeInfo node = findNodeByDescription(rootNode, name);
+
+        if (node == null) {
+            Log.d(TAG, "findViewByNameAndClick fail: " + name);
+        } else {
+            clickNodeAndParent(node);
+        }
+    }
+
+    /**
+     * 点击控件和对应的父控件
+     *
+     * @param node
+     */
+    private void clickNodeAndParent(AccessibilityNodeInfo node) {
+        node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        AccessibilityNodeInfo parent = node.getParent();
+        while (parent != null) {
+            if (parent.isClickable()) {
+                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                break;
+            }
+            parent = parent.getParent();
+        }
+    }
+
+
+    /**
+     * 根据节点的contentDescription找到对应控件. 如果有多个之后返回第一个
+     */
+    public AccessibilityNodeInfo findNodeByDescription(AccessibilityNodeInfo node, String name) {
+        if (node.getChildCount() == 0) {
+            CharSequence charSequence = node.getContentDescription();
+            if (charSequence != null) {
+                String description = String.valueOf(charSequence);
+                Log.d(TAG, "findNodeByStr description:" + description);
+                if (description.contains(name)) {
+                    return node;
+                }
+            }
+        } else {
+            for (int i = 0; i < node.getChildCount(); i++) {
+                if (node.getChild(i) != null) {
+                    AccessibilityNodeInfo ret = findNodeByDescription(node.getChild(i), name);
+                    if (ret != null) {
+                        return ret;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 根据节点名称找到对应控件. 如果有多个只会返回最先找到的一个
+     *
+     * @param node
+     * @param name
+     * @return
+     */
+    public AccessibilityNodeInfo findNodeByName(AccessibilityNodeInfo node, String name) {
+        if (node.getChildCount() == 0) {
+            if (node.getText() != null) {
+                Log.d(TAG, "findNodeByName:" + node.getText().toString());
+                if (name.equals(node.getText().toString())) {
+                    return node;
+                }
+            }
+        } else {
+            for (int i = 0; i < node.getChildCount(); i++) {
+                if (node.getChild(i) != null) {
+                    AccessibilityNodeInfo ret = findNodeByName(node.getChild(i), name);
+                    if (ret != null) {
+                        return ret;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
