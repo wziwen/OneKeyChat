7d1b446f923c63ee0b76ca7efd0768ff121ce2d6
wenziwen <wenzw@zdnst.com>
2018-01-17 17:48:15 +0800

    添加桌面控件

---
+++ app/src/main/java/com/ziwenwen/onekeychat/widget/OneAppWidget.java
@@ -0,0 +1,62 @@
+package com.ziwenwen.onekeychat.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.ziwenwen.onekeychat.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class OneAppWidget extends AppWidgetProvider {
+    private static final String TAG = "OneAppWidget";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.one_app_widget);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        Intent serviceIntent = new Intent(context, GridWidgetService.class);
+        views.setRemoteAdapter(R.id.grid_view, serviceIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        int minWidth = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        int minHeight = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+        int maxWidth  = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+        int maxHeight  = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+        Log.d(TAG, String.format("values: minWidth:%d    minHeight:%d    maxWidth:%d    maxHeight:%d", minWidth, minHeight, maxWidth, maxHeight));
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
+}
+
